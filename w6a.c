54d68d77204960fff51a257c985f6fa98f88cde6
Marek Laskowski <marek.laskowski@matrix.senecac.on.ca>
2016-03-07 08:35:38 -0500

    added initial version of w6a.c

---
+++ w6a.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+/*a function that displays a welcome message */
+
+void welcome(void){
+
+	//your implementation here
+
+}
+
+/*a function that accepts two integer arguments and validates user input to ensure the user enters a number between the two 
+arguments */
+
+int getUserInteger(int minimum, int maximum){
+
+	//your implementation here
+
+}
+
+/*a function that generate a random number between a minimum and maximum number specified by the arguments*/
+
+int generateRandomNumber(int minimum, int maximum){
+
+	//your implementation here
+
+}
+
+/*a function that displays the combat menu*/
+
+void displayMenu(void){
+
+	//your implementation here
+
+}
+
+/*a function that displays a message appropriate for when the player wins the combat*/
+
+void winningMessage(void){
+
+	//your implementation here
+
+}
+
+/*a function that displays a message appropriate for when the player loses the combat*/
+
+void losingMessage(void){
+	//implementation here
+
+}
+
+/*a function that displays the end screen.*/
+
+void gameOver(void){
+
+//your implementation here
+
+}
+
+/* main function - implements combat (a battle) between a monster and a hero
+int main(void){
+
+
+}
