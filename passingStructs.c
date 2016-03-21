2ea4c151e0371aa1f42d723d5c141e5996cc1aaa
Marek Laskowski <marek.laskowski@matrix.senecac.on.ca>
2016-03-21 09:18:21 -0400

    demonstrated passing structs by address, and modifying original value using a function

--- passingStructs.c
+++ passingStructs.c
@@ -22,6 +22,34 @@ void testModifyStruct(struct Weather w){
 	
 }
 
+/*
+this function accepts a struct Weather argument by address (pointer)
+*/
+void testModifyStructPtr(struct Weather *wPtr){
+	/*when you have a pointer to a struct, you have to dereference it FIRST
+	(using the dereference operator *)
+	then you may access any of the fields using the . dot operator
+	it ends up looking like (*pointer_name).field_name
+	this is kind of ugly syntax, and I'll introduce shorthand in moment
+	*/
+	printf("testModifyStructPtr variable wPtr: date %d, temperature %.1f, humidity %.1f\n",
+                         (*wPtr).date, (*wPtr).temperature, (*wPtr).humidity);
+	//modify the date
+	(*wPtr).date = -1;
+	/* 
+	the (*). syntax is kind of confusing. We can use the shorthand -> operator
+	which does both the dereference and field access at the same time ex:
+	*/
+	//modify the humidity and temperature using the -> operator instead
+	wPtr->temperature = -1000;
+	wPtr->humidity = -0.1;
+	printf("testModifyStructPtr variable wPtr (After changes): date %d, temperature %.1f, humidity %.1f\n",
+                         (*wPtr).date, (*wPtr).temperature, (*wPtr).humidity);
+
+
+}
+
+
 int main(void){
 
 	//declare a varaible of our Weather type
@@ -42,7 +70,13 @@ int main(void){
 
 	//call testModifyStruct to see if it will alter today_copy!
 	testModifyStruct(today_copy);
-	printf("today_copy after passing it to testModifyStruct: date %d, temperature %.1f, humidity %.1f\n",
+	printf("today_copy (in main) after passing it to testModifyStruct: date %d, temperature %.1f, humidity %.1f\n",
+                         today_copy.date, today_copy.temperature, today_copy.humidity);
+
+	printf("Now let's test changing a struct when we pass it by address:\n");
+	testModifyStructPtr(&today_copy);
+	printf("today_copy (in main) after passing it to testModifyStructPtr: date %d, temperature %.1f, humidity %.1f\n",
                          today_copy.date, today_copy.temperature, today_copy.humidity);
 
 }
+
